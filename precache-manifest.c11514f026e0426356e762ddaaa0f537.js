ae5400257ca4c9dfc5f860db00eba1ff9de0a144
Maimell <malinka77@gmail.com>
2020-01-04 21:32:42 -0700

    Updates

---
+++ precache-manifest.c11514f026e0426356e762ddaaa0f537.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a596e73fb048bf56caa75cea11dc6e9",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "fb3aa3cf333d9249174b",
+    "url": "/clicky-game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b0b59ca7f76452933317",
+    "url": "/clicky-game/static/js/2.3187ea7e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.3187ea7e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb3aa3cf333d9249174b",
+    "url": "/clicky-game/static/js/main.54dd553b.chunk.js"
+  },
+  {
+    "revision": "b5e3d5480cb174bedaf8",
+    "url": "/clicky-game/static/js/runtime-main.dbb64346.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
